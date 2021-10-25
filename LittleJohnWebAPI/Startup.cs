748d7e216c9d02c6d1ba8d0b8212c8291a010ecc
JordanG <jordangottardo@libero.it>
2021-10-25 22:05:05 +0200

    Code cleanup

--- LittleJohnWebAPI/Startup.cs
+++ LittleJohnWebAPI/Startup.cs
@@ -1,17 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LittleJohnWebAPI.Data;
 using LittleJohnWebAPI.Data.Tickers;
 using LittleJohnWebAPI.Data.Users;
 using LittleJohnWebAPI.Utils;
